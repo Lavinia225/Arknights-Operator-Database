25ec01c3613383b4f15170474d3b29c10fd1817e
luna seifert <thepuddingdeity@gmail.com>
2022-10-21 16:57:39 -0500

    Added basic operator list renderer

--- src/index.js
+++ src/index.js
@@ -6,5 +6,15 @@ searchBar.addEventListener('submit', e=>{
 })
 
 function search(searchingFor = 'operator'){
-    fetch(`http:localhost:3000/operators`).then(res => res.json()).then(operators => console.log(operators))
+    fetch(`http:localhost:3000/operators`).then(res => res.json())
+    .then(operators => operators.forEach(operator => renderOperators(operator)))
+}
+
+function renderOperators(operator){
+    const list = document.getElementById('operatorList')
+    const li = document.createElement('li')
+
+    li.textContent = operator.name
+
+    list.appendChild(li)
 }
